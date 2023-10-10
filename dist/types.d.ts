e30e3934e170b6568875ea78d47c07e572b449fd
陈涛 <jooies@qq.com>
2023-10-10 10:56:34 +0800

    build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,7 +1,16 @@
 export interface RequestConfig extends RequestInit {
+    /**
+     * url prefix, is only work for string type input
+     */
     baseUrl?: string;
     params?: string | Record<string, any>;
     data?: string | Record<string, any> | FormData;
+    /**
+     * how to adjust json request is success
+     */
     success?: (res: Record<string, any>) => boolean;
+    /**
+     * how to handle error
+     */
     error?: (err: unknown) => void;
 }
